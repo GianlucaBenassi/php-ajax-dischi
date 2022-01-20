9468c61e56b7cad0f5b1303616efc7132eade662
Gianluca Benassi <benox1199@gmail.com>
2022-01-20 14:37:51 +0100

    axios call

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        disks: []
+    },
+    created() {
+        axios.get('http://localhost/20220120/php-ajax-dischi/backend.php')
+        .then((response) => {
+            this.disks = response.data;
+        });
+    }
 });
