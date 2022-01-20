40f35c558a1d1a0a9df55cfc098be2ae45ce3913
Gianluca Benassi <benox1199@gmail.com>
2022-01-20 15:31:35 +0100

    genre list

--- script/script.js
+++ script/script.js
@@ -1,12 +1,25 @@
 const app = new Vue({
     el: '#app',
     data: {
-        disks: []
+        disks: [],
+        genres: [],
+        genreSelected: 'All'
     },
     created() {
+
+        // get all disks
         axios.get('http://localhost/20220120/php-ajax-dischi/backend.php')
         .then((response) => {
             this.disks = response.data;
+
+            // create genres list
+            this.disks.forEach(elm => {
+                if (!this.genres.includes(elm.genre)) {
+                    this.genres.push(elm.genre);
+                }
+            });
+
         });
+
     }
 });
